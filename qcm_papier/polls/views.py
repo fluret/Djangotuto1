152136b7a4921fd98d8ba58f7cc749f21d444bed
Frédéric LURET <frederic.luret@univ-tours.fr>
2022-06-02 15:20:59 +0200

    Correction des problèmes de compétitions

--- qcm_papier/polls/views.py
+++ qcm_papier/polls/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.urls import reverse
+from django.db.models import F
 
 from .models import Choice, Question
 
@@ -30,7 +31,7 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
